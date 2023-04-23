54d0700b4135ec6a255259a2b91f41f798ad4b2a
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-23 13:33:15 -0300

    Ainda falta a logica no JavaScript

---
+++ script.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById(`btn`)
+let div = document.getElementById(`content`)
+
+function calc() {
+    let n1 = parseInt(document.getElementById('numero').value)
+
+    if (n1 > 10) {
+        div.innerHTML = `
+            Numero invalido
+        `
+        console.log("numero invalido")
+    } else {
+        for (let i = 20; i <= n1; i++) {          
+           div.innerHTML += `${i}<br>`
+           console.log(i)
+        }
+    }
+}
+btn.addEventListener('click', calc)
+
